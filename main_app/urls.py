05a9d805e6454566f0dd5719699089450047aa68
ranjith.j <ranjithjacob.work@gmail.com>
2026-02-20 12:01:04 +0000

    completes Using Django's ORM in the App pg

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path("about/", views.about, name="about"),
     path('cats/', views.cat_index, name='cat-index'),
+    # new route below
+    path('cats/<int:cat_id>/', views.cat_detail, name='cat-detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from .models import Cat
 
 # Create your views here.
 def home(request):
@@ -11,23 +12,31 @@ def about(request):
     # return HttpResponse("<h1>About the CatCollector</h1>")
     return render(request, "about.html")
 
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+# commented out after schema added
+# class Cat:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
 
+# commented out after schema added
 # Create a list of Cat instances
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
+# cats = [
+#     Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+#     Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+#     Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+#     Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+# ]
 
 def cat_index(request):
     # Render the cats/index.html template with the cats data
+    cats = Cat.objects.all()  # look familiar?
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id) # singular
+    return render(request, 'cats/detail.html', {'cat': cat})
+
 
 
